f4e10503b4f8ed4fa0313dca4443946e609f43e9
Thomas Rose <thomascrose90@gmail.com>
2021-12-09 11:30:50 -0500

    Initial repo structure.

---
+++ client/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render () {
+    return (
+      <div>
+        Hello There!
+      </div>
+    )
+  }
+}
+
+export default App;
