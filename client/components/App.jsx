5c420b3136d1591f9988249e95279a16e158844f
Travis Pak <tpak415@gmail.com>
2021-12-10 14:20:58 -0500

    Set map to default to user address, default to user location if permission granted, Geocoder installed

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,30 +1,31 @@
 import React, {useState, useEffect} from 'react';
+import Geocode from 'react-geocode';
+import { GOOGLE_API_KEY, TEST_USER_ADDRESS } from '../../config.js';
+Geocode.setApiKey(GOOGLE_API_KEY)
+Geocode.setLocationType("ROOFTOP")
 import GoogleMap from './GoogleMap.jsx';
 import SearchBox from './SearchBox.jsx';
 
 
 const App = () => {
   const [state, setState] = useState ({
-    location: {lat: 39, lng: -94},
+    searchedLocation: {},
+    userLocation: {},
+    userAddressLocation: {},
+    userAddress: TEST_USER_ADDRESS, // placeholder - will need to set this to be geocoded form address string to lat/lng object,
     searchBoxText: 'Search a City!'
   })
 
-  const onPlacesChanged = (input) => {
-    // console.log(input);
-  }
-
   const onLocationChange = (lat, lng) => {
-    setState((prevState) => {return {...prevState, location: {lat: lat, lng: lng}}})
+    setState((prevState) => {return {...prevState, searchedLocation: {lat: lat, lng: lng}}})
   }
 
-  const setCurrentLocation = () => {
-    // console.log('finding user position', navigator.geolocation);
+  const getUserLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          // console.log('position: ', position);
-          setState((prevState) =>  { return {
-            location: {
+          setState((prevState) =>  { return {...prevState,
+            userLocation: {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             }
@@ -37,19 +38,29 @@ const App = () => {
     }
   }
 
+  const convertAddressToCoords = (address) => {
+    Geocode.fromAddress(address)
+      .then((response) => {
+        setState((prevState) => { return {...prevState, userAddressLocation: response.results[0].geometry.location}})
+      })
+      .catch((err) => {console.log(err)})
+  }
+
   useEffect(() => {
     // console.log(' initial load: ', state)
-    setCurrentLocation();
+    getUserLocation();
+    convertAddressToCoords(state.userAddress);
   }, [])
 
   useEffect(() => {
     // console.log('updating App state: ', state)
+
   })
 
   return (
     <div>
-      <SearchBox placeholder={state.searchBoxText} onPlacesChanged={onPlacesChanged}/>
-      <GoogleMap location={state.location} onLocationChange={onLocationChange}/>
+      {/* <SearchBox placeholder={state.searchBoxText} onPlacesChanged={onPlacesChanged}/> */}
+      <GoogleMap searchedLocation={state.searchedLocation} userLocation={state.userLocation} userAddressLocation={state.userAddressLocation} onLocationChange={onLocationChange}/>
     </div>
   )
 }
