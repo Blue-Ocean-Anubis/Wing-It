1b7e55f269c92825733681d25a4a5526532bdb07
Travis Pak <tpak415@gmail.com>
2021-12-10 10:25:01 -0500

    Update api file to be config.js

--- client/components/GoogleMap.jsx
+++ client/components/GoogleMap.jsx
@@ -3,7 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 import SearchBox from './SearchBox.jsx';
-import { KEY } from '../../tokens.js';
+import { GOOGLE_API_KEY } from '../../config.js';
 // require('dotenv').config();
 
 const Marker = () => <div><FontAwesomeIcon icon={faMapMarkerAlt} size="2x"/></div>;
@@ -20,7 +20,7 @@ const GoogleMap = (props) => {
       // Important! Always set the container height explicitly
       <div style={{ height: '80vh', width: '90%', margin: '10vh auto 10vh auto'}}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: KEY}}
+          bootstrapURLKeys={{ key: GOOGLE_API_KEY}}
           defaultCenter={{lat: 39, lng: -94}}
           defaultZoom={8}
           onClick={mapClick}
