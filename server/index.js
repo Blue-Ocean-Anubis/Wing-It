d1b2298acfb1d3a6a5d772d6ffe30c8b0aba1792
Thomas Rose <thomascrose90@gmail.com>
2021-12-10 16:42:33 -0500

    Implement Google API in Node. Test library working at particular endpoint.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+const { GOOGLE_API_KEY } = require('../config.js');
+const {Client} = require("@googlemaps/google-maps-services-js");
+const client = new Client({});
 
 require("../db");
 const express = require("express");
@@ -14,6 +17,25 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("dist"));
+
+app.get('/restaurants', (req, res) => {
+  client.findPlaceFromText({
+    params: {
+      input: 'Paris',
+      inputtype: 'textquery',
+      fields: ['name', 'place_id', 'geometry', 'types', 'formatted_address'],
+      key: GOOGLE_API_KEY,
+    },
+    timeout: 1000, // milliseconds
+  })
+  .then((r) => {
+    console.log('SUCCESS: ', r.data.candidates);
+    res.send(r.data.candidates)
+  })
+  .catch((e) => {
+    console.log('ERROR: ', e);
+  });
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
