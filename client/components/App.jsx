7d74175c573eeb5d32668cdec0c03455d362e364
Travis Pak <tpak415@gmail.com>
2021-12-09 20:51:36 -0500

    Fix warnings for map marker, Add function to find user location on mount

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,38 +2,48 @@ import React, {useState, useEffect} from 'react';
 import GoogleMap from './GoogleMap.jsx';
 import SearchBox from './SearchBox.jsx';
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-
-//   render () {
-//     return (
-//       <div>
-//         <GoogleMap />
-//         Hello There!
-//       </div>
-//     )
-//   }
-// }
 
 const App = () => {
   const [state, setState] = useState ({
-    location: {lat: 39.0997, lng: -94.5786},
+    location: {lat: 39, lng: -94},
     searchBoxText: 'Search a City!'
   })
 
   const onPlacesChanged = (input) => {
-    console.log(input);
+    // console.log(input);
   }
 
   const onLocationChange = (lat, lng) => {
     setState((prevState) => {return {...prevState, location: {lat: lat, lng: lng}}})
   }
 
+  const showCurrentLocation = () => {
+    // console.log('finding user position', navigator.geolocation);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // console.log('position: ', position);
+          setState((prevState) =>  { return {
+            location: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            }
+          }
+        })
+        }, (err) => {console.log(err)}
+      )
+    } else {
+      error => console.log(error)
+    }
+  }
+
   useEffect(() => {
-    console.log(state)
+    // console.log(' initial load: ', state)
+    showCurrentLocation();
+  }, [])
+
+  useEffect(() => {
+    // console.log('updating App state: ', state)
   })
 
   return (
--- client/components/GoogleMap.jsx
+++ client/components/GoogleMap.jsx
@@ -2,16 +2,16 @@ import React, {useState} from 'react';
 import GoogleMapReact from 'google-map-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
-import TOKEN from '../../google-API-key-TP.js';
 import SearchBox from './SearchBox.jsx';
+import { KEY } from '../../tokens.js';
+// require('dotenv').config();
 
-
-// const AnyReactComponent = ({  text }) => <div>{text}</div>;
+const Marker = () => <div><FontAwesomeIcon icon={faMapMarkerAlt} size="2x"/></div>;
 
 const GoogleMap = (props) => {
 
   const mapClick = (event) => {
-    console.log(event);
+    // console.log(event);
     props.onLocationChange(event.lat, event.lng)
   }
 
@@ -20,17 +20,12 @@ const GoogleMap = (props) => {
       // Important! Always set the container height explicitly
       <div style={{ height: '80vh', width: '90%', margin: '10vh auto 10vh auto'}}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: TOKEN.KEY }}
-          defaultCenter={props.location}
+          bootstrapURLKeys={{ key: KEY}}
+          defaultCenter={{lat: 39, lng: -94}}
           defaultZoom={8}
           onClick={mapClick}
         >
-          <FontAwesomeIcon
-            icon={faMapMarkerAlt}
-            size="2x"
-            lat={props.location.lat}
-            lng={props.location.lng}
-          />
+          <Marker lat={props.location.lat} lng={props.location.lng} />
         </GoogleMapReact>
 
       </div>
