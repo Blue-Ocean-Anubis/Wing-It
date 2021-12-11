783db4b143cf7cff4b154eea2bccb2aa332ff044
Eric Grochowski <egrochowski42@gmail.com>
2021-12-11 11:48:20 -0500

    Create registration component

--- client/components/auth/registration.jsx
+++ client/components/auth/registration.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
-const Registration = (props) => (
-  <>
-    <label htmlFor="email">email: </label>
-    <input name="email" type="email" onChange={props.handleChange} />
-    <br />
-    <label htmlFor="password">password: </label>
-    <input name="password" type="password" onChange={props.handleChange} />
-    <br />
-    <button>Register</button>
-  </>
-);
+const Registration = (props) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">email: </label>
+      <input id="email" type="email" ref={emailRef} required />
+      <label htmlFor="password">Password: </label>
+      <input id="password" type="password" ref={passwordRef} required />
+      <label htmlFor="confim-password">Confirm password: </label>
+      <input
+        id="confirm-password"
+        type="password"
+        ref={passwordConfirmRef}
+        required
+      />
+      <button>Register</button>
+    </form>
+  );
+};
 export default Registration;
