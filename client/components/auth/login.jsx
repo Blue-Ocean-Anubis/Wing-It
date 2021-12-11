36406779382d02b8f4d7ebffa070f06a785d2998
Eric Grochowski <egrochowski42@gmail.com>
2021-12-11 11:49:19 -0500

    Create login component

--- client/components/auth/login.jsx
+++ client/components/auth/login.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
-const Login = (props) => (
-  <>
-    <label htmlFor="email">email: </label>
-    <input name="email" type="email" onChange={props.handleChange} />
-    <br />
-    <label htmlFor="password">password: </label>
-    <input name="password" type="password" onChange={props.handleChange} />
-    <br />
-    <button>Login</button>
-  </>
-);
+const Login = (props) => {
+  let emailRef = useRef();
+  let passwordRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">email: </label>
+      <input name="email" type="email" ref={emailRef} required />
+      <label htmlFor="password">password: </label>
+      <input name="password" type="password" ref={passwordRef} required />
+      <button>Login</button>
+    </form>
+  );
+};
 
 export default Login;
