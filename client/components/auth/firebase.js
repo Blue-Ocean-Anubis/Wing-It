f87c4d7f6c89a1fc0e045d2cb92da8cdbd3d0359
Eric Grochowski <egrochowski42@gmail.com>
2021-12-11 11:35:57 -0500

    Add config

---
+++ client/components/auth/firebase.js
@@ -0,0 +1,14 @@
+import { intitializeApp } from "firebase/app";
+import "firebase/auth";
+
+const fb = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+};
+
+export const app = app.auth();
+export default fb;
