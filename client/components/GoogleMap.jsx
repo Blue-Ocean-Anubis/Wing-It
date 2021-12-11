ce1acefc74acb0b4178f0070a9e860128fe77b97
Jason Adams <jasonadams1326@gmail.com>
2021-12-10 21:02:36 -0600

    removed unneeded components

--- client/components/GoogleMap.jsx
+++ client/components/GoogleMap.jsx
@@ -2,14 +2,10 @@ import React, {useState} from 'react';
 import GoogleMapReact from 'google-map-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
-import SearchBox from './SearchBox.jsx';
+// import SearchBox from './SearchBox.jsx';
 // require('dotenv').config();
-// import AutoCompleteComponent from './AutoCompleteComponent.jsx';
 import { GOOGLE_API_KEY } from '../../config.js';
-// import PlacesAutocomplete from './PlacesAutocomplete.jsx';
-// import AnotherAutoComplete from './AnotherAutoComplete.jsx';
-import AnotherOne from './AnotherOne.jsx';
-
+import AutoCompleteMapSearch from './AutoCompleteMapSearch.jsx';
 const Marker = () => <div><FontAwesomeIcon icon={faMapMarkerAlt} size="2x"/></div>;
 
 const GoogleMap = (props) => {
@@ -23,19 +19,9 @@ const GoogleMap = (props) => {
 
       // Important! Always set the container height explicitly
       <div style={{ height: '80vh', width: '90%', margin: '10vh auto 10vh auto'}}>
-        {/* <AnotherAutoComplete></AnotherAutoComplete> */}
-        <AnotherOne></AnotherOne>
-        {/* <AutoCompleteComponent
-          apiKey={GOOGLE_API_KEY}
-
-          //accepts API key but seems to need a Typescript interface for below options to work:
-          // autocompletionRequest={{
-          //   types: ['(cities)']
-          // }}
-        >
-        </AutoCompleteComponent> */}
+        <AutoCompleteMapSearch></AutoCompleteMapSearch>
         <GoogleMapReact
-          // bootstrapURLKeys={{ key: GOOGLE_API_KEY}}
+          bootstrapURLKeys={{ key: GOOGLE_API_KEY}}
           defaultCenter={{lat: 39, lng: -94}}
           defaultZoom={8}
           onClick={mapClick}
