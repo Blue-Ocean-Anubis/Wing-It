109eda7b76e5822168e9fad7a19489a52ce78d4a
Eric Grochowski <egrochowski42@gmail.com>
2021-12-11 19:10:15 -0500

    Create Auth context

---
+++ client/components/auth/contexts/AuthContext.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import fb from "./../firebase.js";
+
+const auth = getAuth();
+
+export const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+  // User defaults to null
+  const [user, setUser] = useState(null);
+
+  const signup = (email, password) => {
+    // returns promise
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Run once on mount
+  useEffect(() => {
+    // notfies us whenever the user gets set; listens for state change
+    // user will always be some user or null
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, signup }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
