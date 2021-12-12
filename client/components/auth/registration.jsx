c44149530d813b71c469e7f13913d59b4140fc39
Eric Grochowski <egrochowski42@gmail.com>
2021-12-12 00:16:07 -0500

    Add link to login component

--- client/components/auth/registration.jsx
+++ client/components/auth/registration.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useContext } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "./contexts/AuthContext.jsx";
 
 export function Registration(props) {
@@ -19,19 +20,24 @@ export function Registration(props) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">email: </label>
-      <input id="email" type="email" ref={emailRef} required />
-      <label htmlFor="password">Password: </label>
-      <input id="password" type="password" ref={passwordRef} required />
-      <label htmlFor="confim-password">Confirm password: </label>
-      <input
-        id="confirm-password"
-        type="password"
-        ref={passwordConfirmRef}
-        required
-      />
-      <button>Register</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">email: </label>
+        <input id="email" type="email" ref={emailRef} required />
+        <label htmlFor="password">Password: </label>
+        <input id="password" type="password" ref={passwordRef} required />
+        <label htmlFor="confim-password">Confirm password: </label>
+        <input
+          id="confirm-password"
+          type="password"
+          ref={passwordConfirmRef}
+          required
+        />
+        <button>Register</button>
+      </form>
+      <div>
+        Already have an account? <Link to="/login">Login</Link>
+      </div>
+    </>
   );
 }
