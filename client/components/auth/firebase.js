553160bf4e81df5cf3800f313b9fa0b750e92438
Eric Grochowski <egrochowski42@gmail.com>
2021-12-12 22:53:52 -0500

    Change source of key's values

--- client/components/auth/firebase.js
+++ client/components/auth/firebase.js
@@ -1,14 +1,18 @@
-import { intitializeApp } from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from "firebase/app";
 
-const fb = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
-};
+const fb = initializeApp({
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBIB_P4mT53z--8tcwdXATkd6DoFG86VQw",
+  authDomain: "batbook-dev.firebaseapp.com",
+  projectId: "batbook-dev",
+  storageBucket: "batbook-dev.appspot.com",
+  messagingSenderId: "1066980752772",
+  appId: "1:1066980752772:web:f6cc54112691248670f1d1",
+});
 
-export const auth = app.auth();
 export default fb;
