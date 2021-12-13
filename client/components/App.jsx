85a1963e081c42c9cbb3c7c08d216dafea402a67
Eric Grochowski <egrochowski42@gmail.com>
2021-12-13 18:54:03 -0500

    Add routes for login and register

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,58 +1,46 @@
-
-import Auth from "./auth/Index.jsx";
-import React from 'react';
-import {BrowserRouter, Route, Routes, Switch, Link, useLocation, useHistory} from 'react-router-dom';
+import React, { useContext } from "react";
+import { BrowserRouter, Route, Switch, useLocation } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faHome, faSearch, faShoppingCart} from '@fortawesome/free-solid-svg-icons'
-import Home from './Home.jsx';
-import UserProfile from './UserProfile.jsx';
-import Cart from './Cart.jsx';
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import Home from "./Home.jsx";
+import UserProfile from "./UserProfile.jsx";
+import Cart from "./Cart.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import Registration from "./auth/Registration.jsx";
+import Login from "./auth/Login.jsx";
+import PrivateRoute from "./PrivateRoute.jsx";
 
 const App = () => {
   const location = useLocation();
-
   return (
-      <div className='app'>
-        <nav className="navigation">
-              <Link to="/">
-                <FontAwesomeIcon icon={faHome} size="3x"/>
-              </Link>
-              <Link to="/user">
-                <FontAwesomeIcon icon={faUser} size="3x"/>
-              </Link>
-              <Link to="/search">
-                <FontAwesomeIcon icon={faSearch} size="3x"/>
-              </Link>
-              <Link to="/cart">
-                <FontAwesomeIcon icon={faShoppingCart} size="3x"/>
-              </Link>
-        </nav>
-
-        <Route render={({location}) => (
-          <TransitionGroup>
-          <CSSTransition
-          key={location.key}
-            timeout={300}
-            classNames="fade"
-            >
-              <Switch location={location} key={location.pathname}>
-                <Route path="/user" component={UserProfile}>
-                </Route>
-                <Route path="/cart" component={Cart}>
-                </Route>
-                <Route exact path="/" component={Home}>
-                </Route>
-                <Route exact path="/search" component={Home}>
-                </Route>
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        )} />
-
-      </div>
+    <div className="app">
+      <BrowserRouter>
+        <Route
+          exact
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.key} timeout={300} classNames="fade">
+                <AuthProvider>
+                  <Switch location={location} key={location.pathname}>
+                    <Route path="/user" component={UserProfile}></Route>
+                    <Route path="/cart" component={Cart}></Route>
+                    <PrivateRoute
+                      exact
+                      path="/"
+                      component={Home}
+                    ></PrivateRoute>
+                    <Route exact path="/search" component={Home}></Route>
+                    <Route path="/register" component={Registration}></Route>
+                    <Route path="/login" component={Login}></Route>
+                  </Switch>
+                </AuthProvider>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
+      </BrowserRouter>
+    </div>
   );
-}
+};
 
 export default App;
