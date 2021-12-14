e897d9efab43eb9fc5e4c9f167a33489884ab1f0
Thomas Rose <thomascrose90@gmail.com>
2021-12-14 16:34:28 -0500

    Format POI card.

--- client/components/PointsOfInterestCard.jsx
+++ client/components/PointsOfInterestCard.jsx
@@ -10,9 +10,8 @@ const PointsOfInterestCard = ({ point }) => {
       {<span className="name">{point.name}</span>}
       {<span>{point.rating} of 5</span>}
       {<span>{point.category}</span>}
-      {<span>{point.types.filter(type => type !== 'point_of_interest' && type !== 'establishment')}
-    </span>}
-  </div>
+      {<span>{point.types.filter(type => type !== 'point_of_interest' && type !== 'establishment')}</span>}
+    </div>
   )
 }
 
